6d37c2658830fbf8953011464befd8aaa8e41635
weemsj <71594052+weemsj@users.noreply.github.com>
2022-05-02 11:41:06 +0200

    added routes to app.py and forms to encode_my_life

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 import base64
 import io
 import segno
-from segno import helpers
+from segno import QRCode, helpers
 
 
 app = Flask(__name__)
@@ -19,23 +19,21 @@ def encode_my_life():
 
 @app.route('/text', methods=['POST', 'GET'])
 def text():
-    # txt = request.form['text-input']
-    txt = request.args.get('value')
+    txt = request.form['text-input']
     print(txt)
-    img = segno.make(txt, micro=False)
-    data = io.BytesIO()
-    img.save(data, kind='png', scale=10)
-    img_data = base64.b64encode(data.getvalue())
-    data.seek(0)
-    return send_file(data, mimetype='image/png')
-
-    #return render_template('encode_my_life.j2', code=img_data.decode('utf-8'))
+    #img = segno.make(txt, micro=False)
+    #data = io.BytesIO()
+    #img.save(data, kind='png', scale=10)
+    #img_data = base64.b64encode(data.getvalue())
+    qrcode = segno.make(txt, micro=False)
+    return render_template( 'encode_my_life.html', qrcode=qrcode)
+    #return render_template('encode_my_life.html', txt=txt, code=img_data.decode('utf-8'))
 
 @app.route('/url')
 def url():
     text= request.form['url-input']
-    
-    return render_template('encode_my_life.html', code=text)
+    qrcode = segno.make(text, micro=False)
+    return render_template('encode_my_life.html', qrcode=qrcode)
 
 
 
@@ -43,13 +41,41 @@ def url():
 def wifi():
     ssid = request.form['ssid']
     password = request.form['password'] 
-    security = request.form['security']
-    qrcode_data = helpers.make_wifi(ssid=ssid, password=password, security=security)
-    data = io.BytesIO()
-    qrcode_data.save(data, kind='png', scale=10)
-    qrcode_data.save('.png')
+    security = request.form['inlineRadioSecurity']
+    if security == "None":
+        security = None
+    qrcode = helpers.make_wifi(ssid=ssid, password=password, security=security, hidden=False)
+    print(qrcode)
+    #qrcode = segno.make_qr(qrcode_data, micro=False, error='h')
     #img_data = base64.b64decode(data.getvalue())
-    #return render_template('encode_my_life.j2', code=img_data.decode('utf-8'))
+    return render_template('encode_my_life.html', qrcode=qrcode)
+    
+
+@app.route('/meCard', methods=['POST'])
+def meCard():
+    
+    name= request.form['lastName'] + "," + request.form['firstName']
+    email= request.form['email']
+    primaryTel= request.form['primaryTel']
+    secondaryTel= request.form['secondaryTel']
+    nickname= request.form['nickname']
+    birthday= request.form['birthDate'] # YYYY-MM-DD
+    url= request.form['website']
+    
+    vals = [name, email, primaryTel, secondaryTel, nickname, birthday, url]
+    
+    for v in vals:
+        if v == "":
+            v = None
+            
+    if secondaryTel != None:
+        tel = (primaryTel, secondaryTel)
+    else:
+        tel = primaryTel        
+        
+    qrcode = helpers.make_mecard(name=name, email=email, phone=tel, nickname=nickname, birthday=birthday, url=url)
+    return render_template('encode_my_life.html', qrcode=qrcode)
+
     
 
 @app.route('/save', methods=['POST'])
